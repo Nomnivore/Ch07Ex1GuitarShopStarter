f9760d2441c0773227730349524e77172f49011e
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-29 17:26:22 -0500

    hands on 3

--- GuitarShop/Controllers/ProductController.cs
+++ GuitarShop/Controllers/ProductController.cs
@@ -31,6 +31,13 @@ namespace GuitarShop.Controllers
                 products = context.Products
                     .OrderBy(p => p.ProductID).ToList();
             }
+            else if (id == "Strings")
+            {
+                products = context.Products
+                    .Where(p => p.Category.Name == "Guitars" || p.Category.Name == "Basses")
+                    .OrderBy(p => p.ProductID)
+                    .ToList();
+            }
             else
             {
                 products = context.Products
@@ -40,7 +47,10 @@ namespace GuitarShop.Controllers
 
             // use ViewBag to pass data to view
             ViewBag.Categories = categories;
-            ViewBag.SelectedCategoryName = id;
+            if (id == "Strings")
+                ViewBag.SelectedCategoryName = "Strings";
+            else
+                ViewBag.SelectedCategoryName = id;
 
             // bind products to view
             return View(products);
